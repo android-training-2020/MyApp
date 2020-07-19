7e017f5c2da44c7d7c43e6df1c26a99c86ce17dc
Dong Yuwei <newdongyuwei@gmail.com>
2020-07-20 00:08:52 +0800

    login view layout

--- app/src/main/java/us/erlang/myapp/ConstraintLayoutActivity.java
+++ app/src/main/java/us/erlang/myapp/ConstraintLayoutActivity.java
@@ -2,7 +2,9 @@ package us.erlang.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConstraintLayoutActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ConstraintLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
+    }
+
+    public void gotoLoginView(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
